1b60d4a66701045cb1a6135b183c77275745a50f
cerenbilgeseferoglu <48280989+cerenbilgeseferoglu@users.noreply.github.com>
2020-12-05 23:05:43 +0300

    Update cerenbilgeseferoglu_02.sql

--- cerenbilgeseferoglu_02.sql
+++ cerenbilgeseferoglu_02.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: 127.0.0.1
--- Üretim Zamanı: 28 Kas 2020, 15:34:18
+-- Üretim Zamanı: 05 Ara 2020, 21:03:56
 -- Sunucu sürümü: 10.4.14-MariaDB
 -- PHP Sürümü: 7.4.11
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Veritabanı: `cerenbilgeseferoglu_01`
 --
+CREATE DATABASE IF NOT EXISTS `cerenbilgeseferoglu_01` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `cerenbilgeseferoglu_01`;
 
 -- --------------------------------------------------------
 
@@ -28,9 +30,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `eser` (
+  `EsKayNo` smallint(6) NOT NULL COMMENT 'Kitap Kayıt Numarası\r\n',
   `EsISBN` varchar(13) COLLATE utf8_unicode_ci NOT NULL,
   `EsAdi` varchar(200) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Eserin Adı',
-  `EsKayNo` smallint(6) NOT NULL COMMENT 'Kitap Kayıt Numarası\r\n',
   `EsDil` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `EsKayTar` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Kitabın Veri Tabanına kaydedildiği tarih',
   `YayineviNo` int(11) NOT NULL COMMENT 'Yayınevi No',
@@ -41,17 +43,17 @@ CREATE TABLE `eser` (
 -- Tablo döküm verisi `eser`
 --
 
-INSERT INTO `eser` (`EsISBN`, `EsAdi`, `EsKayNo`, `EsDil`, `EsKayTar`, `YayineviNo`, `YazNo`) VALUES
-('9786059503532', 'Çevre imtihanımız: ekoloji bağlamında', 1, 'Türkçe', '2020-11-02 10:56:16', 1, 1),
-('9786058133990', 'Kent ve ekoloji araştırmaları için internetten kamusal veriye ulaşma rehberi 2019', 2, 'Türkçe', '2020-11-02 11:03:57', 2, 2),
-('9789751414137', 'Çevre ve ekoloji', 3, 'Türkçe', '2020-11-02 11:08:22', 3, 3),
-('9786054613878', 'Biyocoğrafya & Bir Paleocoğrafya ve Ekoloji Yaklaşımı', 4, 'Türkçe', '2020-11-02 11:13:52', 4, 4),
-('9789759637705', 'Ekoloji', 5, 'Türkçe', '2020-11-02 11:17:17', 5, 5),
-('9789758109432', 'Ekoloji Terimleri Sözlüğü', 6, 'Türkçe', '2020-11-02 11:19:50', 6, 6),
-('9789759493905', 'Çevre bilim: Ekoloji', 7, 'Türkçe', '2020-11-02 11:21:32', 7, 7),
-('9789759225711', ' Ekoloji, toplum ve siyaset', 8, 'Türkçe', '2020-11-02 11:23:14', 8, 8),
-('9789754831771', 'Ekoloji ve çevre biyolojisi', 9, 'Türkçe', '2020-11-02 11:24:23', 9, 9),
-('9786054445219', 'Çevre sağlığı : (çevre ve ekoloji bağlantılarıyla)', 10, 'Türkçe', '2020-11-02 11:27:19', 10, 10);
+INSERT INTO `eser` (`EsKayNo`, `EsISBN`, `EsAdi`, `EsDil`, `EsKayTar`, `YayineviNo`, `YazNo`) VALUES
+(1, '9786059503532', 'Çevre imtihanımız: ekoloji bağlamında', 'Türkçe', '2020-11-02 10:56:16', 1, 1),
+(2, '9786058133990', 'Kent ve ekoloji araştırmaları için internetten kamusal veriye ulaşma rehberi 2019', 'Türkçe', '2020-11-02 11:03:57', 2, 2),
+(3, '9789751414137', 'Çevre ve ekoloji', 'Türkçe', '2020-11-02 11:08:22', 3, 3),
+(4, '9786054613878', 'Biyocoğrafya & Bir Paleocoğrafya ve Ekoloji Yaklaşımı', 'Türkçe', '2020-11-02 11:13:52', 4, 4),
+(5, '9789759637705', 'Ekoloji', 'Türkçe', '2020-11-02 11:17:17', 5, 5),
+(6, '9789758109432', 'Ekoloji Terimleri Sözlüğü', 'Türkçe', '2020-11-02 11:19:50', 6, 6),
+(7, '9789759493905', 'Çevre bilim: Ekoloji', 'Türkçe', '2020-11-02 11:21:32', 7, 7),
+(8, '9789759225711', ' Ekoloji, toplum ve siyaset', 'Türkçe', '2020-11-02 11:23:14', 8, 8),
+(9, '9789754831771', 'Ekoloji ve çevre biyolojisi', 'Türkçe', '2020-11-02 11:24:23', 9, 9),
+(10, '9786054445219', 'Çevre sağlığı : (çevre ve ekoloji bağlantılarıyla)', 'Türkçe', '2020-11-02 11:27:19', 10, 10);
 
 -- --------------------------------------------------------
 
@@ -61,14 +63,10 @@ INSERT INTO `eser` (`EsISBN`, `EsAdi`, `EsKayNo`, `EsDil`, `EsKayTar`, `Yayinevi
 --
 CREATE TABLE `liste` (
 `EsAdi` varchar(200)
-,`EsISBN` varchar(13)
-,`EsDil` varchar(100)
-,`EsKayTar` timestamp
-,`YayinYer` varchar(100)
-,`YayinTar` year(4)
+,`AdSoyad` varchar(200)
 ,`YayineviAd` varchar(100)
-,`AdSoyad` varchar(201)
-,`YazNo` smallint(11)
+,`YayinTar` year(4)
+,`YayinYer` varchar(100)
 );
 
 -- --------------------------------------------------------
@@ -78,27 +76,27 @@ CREATE TABLE `liste` (
 --
 
 CREATE TABLE `yayin` (
+  `YayineviNo` int(11) NOT NULL COMMENT 'Yayınevi No',
   `YayineviAd` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Yayınevinin Adı',
   `YayinTar` year(4) DEFAULT NULL COMMENT 'Yayın Tarihi',
-  `YayinYer` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Yayın Yeri',
-  `YayineviNo` int(11) NOT NULL COMMENT 'Yayınevi No'
+  `YayinYer` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Yayın Yeri'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Yayınevine Ait Özellikler';
 
 --
 -- Tablo döküm verisi `yayin`
 --
 
-INSERT INTO `yayin` (`YayineviAd`, `YayinTar`, `YayinYer`, `YayineviNo`) VALUES
-('Araştırma Yayınları', 2019, 'Ankara', 1),
-('Ekoloji Kolektifi Derneği', 2019, 'İstanbul', 2),
-('Remzi Kitabevi', 2010, 'İstanbul', 3),
-('Kriter Basım Yayın Dağıtım', 2015, 'Ankara', 4),
-('Başkent Klişe Matbaacılık', 1997, 'Ankara', 5),
-('Birleşik Dağıtım', 2000, 'istanbul', 6),
-('Gazi Kitabevi', 1999, 'Ankara', 7),
-('Odak', 2003, 'Ankara', 8),
-('Ege Üniversitesi', 1997, 'İzmir', 9),
-('Yazıt Yayıncılık', 2012, 'Ankara', 10);
+INSERT INTO `yayin` (`YayineviNo`, `YayineviAd`, `YayinTar`, `YayinYer`) VALUES
+(1, 'Araştırma Yayınları', 2019, 'Ankara'),
+(2, 'Ekoloji Kolektifi Derneği', 2019, 'İstanbul'),
+(3, 'Remzi Kitabevi', 2010, 'İstanbul'),
+(4, 'Kriter Basım Yayın Dağıtım', 2015, 'Ankara'),
+(5, 'Başkent Klişe Matbaacılık', 1997, 'Ankara'),
+(6, 'Birleşik Dağıtım', 2000, 'istanbul'),
+(7, 'Gazi Kitabevi', 1999, 'Ankara'),
+(8, 'Odak', 2003, 'Ankara'),
+(9, 'Ege Üniversitesi', 1997, 'İzmir'),
+(10, 'Yazıt Yayıncılık', 2012, 'Ankara');
 
 -- --------------------------------------------------------
 
@@ -107,27 +105,27 @@ INSERT INTO `yayin` (`YayineviAd`, `YayinTar`, `YayinYer`, `YayineviNo`) VALUES
 --
 
 CREATE TABLE `yazar` (
-  `YazAdı` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `YazSoyad` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `YazNo` smallint(11) NOT NULL COMMENT 'Yazara Ait Numara Bilgisi',
-  `YazUrl` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Yazar Url'
+  `YazUrl` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Yazar Url',
+  `YazAdi` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `YazSoyad` varchar(100) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Yazara ait özellikler';
 
 --
 -- Tablo döküm verisi `yazar`
 --
 
-INSERT INTO `yazar` (`YazAdı`, `YazSoyad`, `YazNo`, `YazUrl`) VALUES
-('Süleyman ', 'Kaya', 1, NULL),
-('Elif ', 'İnce', 2, NULL),
-('Mine ', 'Kışlalıoğlu', 3, NULL),
-('Murat', 'Türkeş', 4, 'https://www.researchgate.net/profile/Murat_Tuerkes2'),
-('Ayşe', 'Boşgelmez', 5, NULL),
-('Beyhan ', 'İslam', 6, NULL),
-('M. Nihat', 'Şişli', 7, NULL),
-('Tuncay', 'Önder', 8, 'https://scholar.google.com.tr/citations?user=j3qWKsYAAAAJ&hl=tr'),
-('Ahmet ', 'Kocataş', 9, NULL),
-('Çağatay', 'Güler', 10, 'http://www.halksagligi.hacettepe.edu.tr/personel/guler.php');
+INSERT INTO `yazar` (`YazNo`, `YazUrl`, `YazAdi`, `YazSoyad`) VALUES
+(1, NULL, 'Süleyman ', 'Kaya'),
+(2, NULL, 'Elif ', 'İnce'),
+(3, NULL, 'Mine ', 'Kışlalıoğlu'),
+(4, 'https://www.researchgate.net/profile/Murat_Tuerkes2', 'Murat', 'Türkeş'),
+(5, NULL, 'Ayşe', 'Boşgelmez'),
+(6, NULL, 'Beyhan ', 'İslam'),
+(7, NULL, 'M. Nihat', 'Şişli'),
+(8, 'https://scholar.google.com.tr/citations?user=j3qWKsYAAAAJ&hl=tr', 'Tuncay', 'Önder'),
+(9, NULL, 'Ahmet ', 'Kocataş'),
+(10, 'http://www.halksagligi.hacettepe.edu.tr/personel/guler.php', 'Çağatay', 'Güler');
 
 -- --------------------------------------------------------
 
@@ -136,7 +134,7 @@ INSERT INTO `yazar` (`YazAdı`, `YazSoyad`, `YazNo`, `YazUrl`) VALUES
 --
 DROP TABLE IF EXISTS `liste`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `liste`  AS SELECT `k`.`EsAdi` AS `EsAdi`, `k`.`EsISBN` AS `EsISBN`, `k`.`EsDil` AS `EsDil`, `k`.`EsKayTar` AS `EsKayTar`, `y`.`YayinYer` AS `YayinYer`, `y`.`YayinTar` AS `YayinTar`, `y`.`YayineviAd` AS `YayineviAd`, concat(`r`.`YazAdı`,' ',`r`.`YazSoyad`) AS `AdSoyad`, `r`.`YazNo` AS `YazNo` FROM ((`eser` `k` join `yayin` `y`) join `yazar` `r`) WHERE `k`.`YayineviNo` = `y`.`YayineviNo` AND `k`.`YazNo` = `r`.`YazNo` ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `liste`  AS SELECT `k`.`EsAdi` AS `EsAdi`, concat(`r`.`YazAdi`,'',`r`.`YazSoyad`) AS `AdSoyad`, `y`.`YayineviAd` AS `YayineviAd`, `y`.`YayinTar` AS `YayinTar`, `y`.`YayinYer` AS `YayinYer` FROM ((`eser` `k` join `yayin` `y`) join `yazar` `r`) WHERE `k`.`YayineviNo` = `y`.`YayineviNo` AND `k`.`YazNo` = `r`.`YazNo` ;
 
 --
 -- Dökümü yapılmış tablolar için indeksler
@@ -146,7 +144,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 -- Tablo için indeksler `eser`
 --
 ALTER TABLE `eser`
-  ADD PRIMARY KEY (`EsKayNo`);
+  ADD PRIMARY KEY (`EsKayNo`) USING BTREE;
 
 --
 -- Tablo için indeksler `yayin`
